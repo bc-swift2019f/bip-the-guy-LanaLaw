3b958cffe225ba318112411f929af15b291cec6e
Ellana Lawrence <elawrence.blain@gmail.com>
2019-09-16 08:47:41 -0400

    Initial Commit

---
+++ Official Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Official Bip the Guy
+//
+//  Created by Ellana Lawrence on 9/16/19.
+//  Copyright © 2019 Ellana Lawrence. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
